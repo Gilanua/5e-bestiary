d66d01ee8c3c3e5a79488f8ab5983ef9f23ef4c9
Aista <aista@outlook.com>
2018-03-24 17:45:33 +0300

    Remove unused elements, replace switch with object

--- src/App.js
+++ src/App.js
@@ -4,31 +4,6 @@ import styled from 'styled-components';
 import {Creature} from './components/creature/Creature';
 import data from './components/fixture.json';
 
-const AppBody = styled.div`
-`
-const AppLogo = styled.img`
-  animation: App-logo-spin infinite 20s linear;
-  height: 80px;
-  @keyframes App-logo-spin {
-    from { transform: rotate(0deg); }
-    to { transform: rotate(360deg); }
-  }
-`
-const AppHeader = styled.header`
-  background-color: #222;
-  height: 150px;
-  padding: 20px;
-  color: white;
-`
-
-const AppTitle = styled.h1`
-  font-size: 1.5em;
-`
-
-const AppIntro = styled.p`
-  font-size: large;
-`
-
 class App extends Component {
   render() {
     return (
--- src/components/creature/Creature.js
+++ src/components/creature/Creature.js
@@ -135,54 +135,41 @@ function translateSize(key) {
 
 function getAbilityModificator(val) {
   val = +val;
-  switch (val) {
-    case 1:
-      return "-5";
-    case 2:
-    case 3:
-      return "-4";
-    case 4:
-    case 5:
-      return "-3";
-    case 6:
-    case 7:
-      return "-2";
-    case 8:
-    case 9:
-      return "-1";
-    case 10:
-    case 11:
-      return "+0";
-    case 12:
-    case 13:
-      return "+1";
-    case 14:
-    case 15:
-      return "+2";
-    case 16:
-    case 17:
-      return "+3";
-    case 18:
-    case 19:
-      return "+4";
-    case 20:
-    case 21:
-      return "+5";
-    case 22:
-    case 23:
-      return "+6";
-    case 24:
-    case 25:
-      return "+7";
-    case 26:
-    case 27:
-      return "+8";
-    case 28:
-    case 29:
-      return "+9";
-    case 30:
-      return "+10";
+
+  const mods = {
+    1: "-5",
+    2: "-4",
+    3: "-4",
+    4: "-3",
+    5: "-3",
+    6: "-2",
+    7: "-2",
+    8: "-1",
+    9: "-1",
+    10: "+0",
+    11: "+0",
+    12: "+1",
+    13: "+1",
+    14: "+2",
+    15: "+2",
+    16: "+3",
+    17: "+3",
+    18: "+4",
+    19: "+4",
+    20: "+5",
+    21: "+5",
+    22: "+6",
+    23: "+6",
+    24: "+7",
+    25: "+7",
+    26: "+8",
+    27: "+8",
+    28: "+9",
+    29: "+9",
+    30: "+10",
   }
+
+  return mods[val];
 }
 
 function getChallengeRateXp(rate) {
